73d75fa94169dbadf5c9854cc241a6cf8f5eb77b
Zamaraev Arseny <ars.zamaraev@gmail.com>
2021-12-15 12:36:37 +0300

    Added Main class

---
+++ src/bsu/rfe/java/group6/lab4/ZAMARAEV/var13A/Main.java
@@ -0,0 +1,15 @@
+package bsu.rfe.java.group6.lab4.ZAMARAEV.var13A;
+
+import javax.swing.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Создать и показать экземпляр главного окна приложения
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+}
